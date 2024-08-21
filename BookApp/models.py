a157bc03f8679a309c4efe3f907d98c544e9d39c
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-08-21 09:12:11 +0300

    commeting the tables

--- BookApp/models.py
+++ BookApp/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import CASCADE
 
 # Create your models here.
 
 
 class BookModel(models.Model):
     name = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        "auth.User", max_length=100, default=None, null=True, on_delete=CASCADE
+    )
     price = models.IntegerField(default=0)
